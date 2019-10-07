55d681c4656af386ddd99efd79c220ecfeac20ac
Idsarth <idsarthdev19@gmail.com>
2019-10-07 11:00:25 -0600

    modificaciones al proyecto

---
+++ src/utils/keys.js
@@ -0,0 +1,10 @@
+export const keys = {
+  asyncStorageKey: 'skiperStorage',
+  urlApi: 'https://serene-lake-64396.herokuapp.com/v1/api/',
+  googleMaps: {
+    apiKey: 'AIzaSyD_S3b75tC_Td7aq8oQLsr5-VX9FO1v2yc',
+    autocomplete: 'https://maps.googleapis.com/maps/api/place/autocomplete/',
+    directions: 'https://maps.googleapis.com/maps/api/directions/',
+    geocode: 'https://maps.googleapis.com/maps/api/geocode/'
+  }
+}
