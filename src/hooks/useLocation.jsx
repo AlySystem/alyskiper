fec16cfffa47db74305f5a09ea295fa5c08d7119
Idsarth <idsarthdev19@gmail.com>
2019-11-11 12:11:34 -0600

    refactorizado los hooks de location

--- src/hooks/useLocation.jsx
+++ src/hooks/useLocation.jsx
@@ -2,33 +2,18 @@ import { useState, useEffect } from 'react'
 import Geolocation from 'react-native-geolocation-service'
 import { useDispatch } from 'react-redux'
 
-// import utils
-import { hasLocationPermission } from '../utils/PermissionLocation'
-
-// Import actions type
-import { LOCATION } from '../store/actionTypes'
-
 export const useLocation = () => {
   const dispatch = useDispatch()
   const [isLoading, setIsLoading] = useState(true)
+  const [location, setLocation] = useState()
   const [errorRegion, setErrorRegion] = useState(null)
-  let watchId = null
 
   useEffect(() => {
     const fetchLocation = async () => {
-      const location = await hasLocationPermission()
-      if (!location) return
       setIsLoading(true)
-
-      watchId = Geolocation.watchPosition(
+      Geolocation.getCurrentPosition(
         ({ coords: { latitude, longitude } }) => {
-          dispatch({
-            type: LOCATION,
-            payload: {
-              latitude,
-              longitude
-            }
-          })
+          setLocation({ latitude, longitude, latitudeDelta: 0.0143, longitudeDelta: 0.0134 })
           setIsLoading(false)
         }, error => {
           setErrorRegion(error)
@@ -36,11 +21,7 @@ export const useLocation = () => {
       )
     }
     fetchLocation()
+  }, [dispatch])
 
-    return () => {
-      Geolocation.clearWatch(watchId)
-    }
-  }, [watchId, dispatch])
-
-  return { errorRegion, isLoading }
+  return { errorRegion, isLoading, location }
 }
