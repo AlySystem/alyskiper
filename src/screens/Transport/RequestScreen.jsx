942214be2ff95ceca40eacdef36d9325f3646863
Idsarth <idsarthdev19@gmail.com>
2019-12-19 03:35:23 -0600

    mimplementado geolib para establecer un radio y obtener al drive mas cercano

--- src/screens/Transport/RequestScreen.jsx
+++ src/screens/Transport/RequestScreen.jsx
@@ -8,9 +8,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useMutation } from '@apollo/react-hooks'
 import { showMessage } from 'react-native-flash-message'
 import PublicIp from 'react-native-public-ip'
+import { isPointWithinRadius, orderByDistance } from 'geolib'
 
 // Import querys
-import { GETDRIVERNEARBY, GENERATETRAVEL } from '../../graphql/mutations/Mutations'
+import { GENERATETRAVEL } from '../../graphql/mutations/Mutations'
 
 // Import actions
 import { REMOVEDETAILSTRAVEL, REMOVELOCATION } from '../../store/actionTypes'
@@ -28,6 +29,7 @@ import Loader from '../../components/loader/Loader'
 import { Theme } from '../../constants/Theme'
 
 const RequestScreen = props => {
+
   const dispatch = useDispatch()
   const { goBack, navigate } = props.navigation
   const { userId } = useSelector(state => state.user)
@@ -35,9 +37,26 @@ const RequestScreen = props => {
   const { steps } = useSelector(state => state.direction)
   const { latitude, longitude } = useSelector(state => state.location)
   const { silver, golden, vip, president } = useSelector(state => state.drivers)
-  const [GetDriverNearby, { error }] = useMutation(GETDRIVERNEARBY)
-  const [GenerateTravel] = useMutation(GENERATETRAVEL)
+  const [GenerateTravel, { error, loading, data }] = useMutation(GENERATETRAVEL)
   useNotification(navigate, latitude, longitude)
+
+  if (error) {
+    showMessage({
+      message: 'Error',
+      description: `${error}`,
+      backgroundColor: 'red',
+      color: '#fff',
+      icon: 'danger',
+      duration: 8000,
+      titleStyle: {
+        fontFamily: 'Lato-Bold'
+      },
+      textStyle: {
+        fontFamily: 'Lato-Regular'
+      }
+    })
+    return goBack()
+  }
 
   const handleOnCancel = () => {
     dispatch({
@@ -51,122 +70,77 @@ const RequestScreen = props => {
   }
 
   useEffect(() => {
-    const finalArray = []
+    const driverWithInRadius = []
     const { categoryId } = travel
+    let orderDistance = null
+    let driverNearby = null
+    const { duration, distance, end_address, start_address, start_location, end_location } = steps
+
     switch (categoryId) {
       case 1:
         silver.map(drive => {
-          return finalArray.push({
-            iddrive: drive.state.SkiperAgentId,
-            lat: drive.state.coords.latitude,
-            lng: drive.state.coords.longitude
-          })
+          if (isPointWithinRadius({ latitude, longitude }, { latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude }, 6000)) {
+            driverWithInRadius.push({ driveId: drive.state.SkiperAgentId, latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude })
+          }
         })
+        orderDistance = orderByDistance({ latitude, longitude }, driverWithInRadius)
+        driverNearby = orderDistance[0]
         break
       case 2:
         golden.map(drive => {
-          return finalArray.push({
-            iddrive: drive.state.SkiperAgentId,
-            lat: drive.state.coords.latitude,
-            lng: drive.state.coords.longitude
-          })
+          if (isPointWithinRadius({ latitude, longitude }, { latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude }, 6000)) {
+            driverWithInRadius.push({ driveId: drive.state.SkiperAgentId, latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude })
+          }
         })
+        orderDistance = orderByDistance({ latitude, longitude }, driverWithInRadius)
+        driverNearby = orderDistance[0]
         break
       case 3:
         vip.map(drive => {
-          return finalArray.push({
-            iddrive: drive.state.SkiperAgentId,
-            lat: drive.state.coords.latitude,
-            lng: drive.state.coords.longitude
-          })
+          if (isPointWithinRadius({ latitude, longitude }, { latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude }, 6000)) {
+            driverWithInRadius.push({ driveId: drive.state.SkiperAgentId, latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude })
+          }
         })
+        orderDistance = orderByDistance({ latitude, longitude }, driverWithInRadius)
+        driverNearby = orderDistance[0]
         break
       case 4:
         president.map(drive => {
-          return finalArray.push({
-            iddrive: drive.state.SkiperAgentId,
-            lat: drive.state.coords.latitude,
-            lng: drive.state.coords.longitude
-          })
+          if (isPointWithinRadius({ latitude, longitude }, { latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude }, 6000)) {
+            driverWithInRadius.push({ driveId: drive.state.SkiperAgentId, latitude: drive.state.coords.latitude, longitude: drive.state.coords.longitude })
+          }
         })
+        orderDistance = orderByDistance({ latitude, longitude }, driverWithInRadius)
+        driverNearby = orderDistance[0]
         break
     }
 
-    GetDriverNearby({
-      variables: {
-        lat: latitude, lng: longitude, inputdrive: finalArray
-      }
-    })
-      .then(({ data }) => {
-        const driverId = data.ObtenerDriveCercano
-        const { categoryId } = travel
-        const { duration, distance, end_address, start_address, start_location, end_location } = steps
+    console.log(driverNearby.driverId)
 
-        PublicIp().then(
-          _ip => {
-            GenerateTravel({
-              variables: {
-                inputviaje: {
-                  idusers: userId,
-                  iddriver: driverId,
-                  lat_initial: start_location.lat,
-                  lng_initial: start_location.lng,
-                  lat_final: end_location.lat,
-                  lng_final: end_location.lng,
-                  distance: parseInt(distance.value),
-                  time: duration.value,
-                  address_initial: start_address,
-                  address_final: end_address,
-                  idcurrency: 2,
-                  idpayment_methods: 2,
-                  categoryId: categoryId
-                },
-                ip: _ip
-              }
-            }).catch(error => {
-                showMessage({
-                  message: 'Error',
-                  description: `${error}`,
-                  backgroundColor: 'red',
-                  color: '#fff',
-                  icon: 'danger',
-                  duration: 8000,
-                  titleStyle: {
-                    fontFamily: 'Lato-Bold'
-                  },
-                  textStyle: {
-                    fontFamily: 'Lato-Regular'
-                  }
-                })
-                goBack()
-              })
-          }
-         )        
-      })
-  }, [])
-
-  useEffect(() => {
-    const verifyError = () => {
-      if (error) {
-        showMessage({
-          message: 'Error',
-          description: `${error.graphQLErrors[0].message} en tu zona para esta categoria, por favor seleccione otra categoria.`,
-          backgroundColor: 'red',
-          color: '#fff',
-          icon: 'danger',
-          duration: 12000,
-          titleStyle: {
-            fontFamily: 'Lato-Bold'
-          },
-          textStyle: {
-            fontFamily: 'Lato-Regular'
+    PublicIp().then(
+      _ip => {
+        GenerateTravel({
+          variables: {
+            inputviaje: {
+              idusers: userId,
+              iddriver: driverNearby.driveId,
+              lat_initial: start_location.lat,
+              lng_initial: start_location.lng,
+              lat_final: end_location.lat,
+              lng_final: end_location.lng,
+              distance: parseInt(distance.value),
+              time: duration.value,
+              address_initial: start_address,
+              address_final: end_address,
+              idcurrency: 2,
+              idpayment_methods: 2,
+              categoryId: categoryId
+            },
+            ip: _ip
           }
         })
-        goBack()
-      }
-    }
-    verifyError()
-  }, [error])
+      })
+  }, [])
 
   return (
     <Background>
