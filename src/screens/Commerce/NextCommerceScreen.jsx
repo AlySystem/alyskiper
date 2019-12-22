6a5ec60e42babd66c2c1cf83b38ebfc7050a1ac4
Idsarth <idsarthdev19@gmail.com>
2019-12-22 17:11:54 -0600

    agregado las promociones a los comercios de proximamente

--- src/screens/Commerce/NextCommerceScreen.jsx
+++ src/screens/Commerce/NextCommerceScreen.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    ScrollView
 } from 'react-native'
 
 // Import components
 import Background from '../../components/background/Background'
+import Title from '../../components/title/Title'
+import Promotion from '../../components/promotion/Promotion'
 
 // Import containers
 import ListOfSwiper from '../../containers/ListOfSwiper'
+
+// Import theme
+import { Theme } from '../../constants/Theme'
+
+const items = [
+    {
+        key: 1,
+        img: 'https://storage.googleapis.com/app_user_bucket/apie.jpg'
+    },
+    {
+        key: 2,
+        img: 'https://storage.googleapis.com/app_user_bucket/bici.jpg'
+    },
+    {
+        key: 3,
+        img: 'https://storage.googleapis.com/app_user_bucket/moto.jpg'
+    },
+    {
+        key: 4,
+        img: 'https://storage.googleapis.com/app_user_bucket/taxi.jpg'
+    }
+]
 
 const NextCommerceScreen = props => {
     return (
@@ -16,6 +41,20 @@ const NextCommerceScreen = props => {
             <View style={styles.screen}>
                 <View style={styles.card}>
                     <ListOfSwiper />
+                </View>
+                <View style={styles.container}>
+                    <Title
+                        title='Promociones'
+                        styles={styles.title}
+                    />
+                    <ScrollView horizontal keyboardShouldPersistTaps='always'>
+                        {items.map((item, index) => (
+                            <Promotion
+                                key={index}
+                                source={{ uri: item.img }}
+                            />
+                        ))}
+                    </ScrollView>
                 </View>
             </View>
         </Background>
@@ -29,7 +68,12 @@ const styles = StyleSheet.create({
     },
     card: {
         height: 230,
-        marginVertical: 12
+        marginVertical: 0
+    },
+    title: {
+        color: Theme.COLORS.colorSecondary,
+        fontSize: Theme.SIZES.title,
+        fontFamily: 'Lato-Bold'
     }
 })
 
